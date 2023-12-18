9a19247296ac2b189edcfd3e4811491a8ccab161
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-18 19:37:56 +0500

    Add AppLayout component

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './Header';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet } from 'react-router-dom';
+
+function AppLayout() {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <h1>Context</h1>
+        <Outlet /> 
+      </main>
+
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <header>
+      <Link to='/'>Fast React PIzza Co.</Link>
+
+      <p>Huzaifa</p>
+    </header>
+  );
+}
+
+export default Header
