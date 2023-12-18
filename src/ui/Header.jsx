9cdb649712f3a194900b6df9f9253ee80a5f6da6
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-18 20:26:54 +0500

    Fetch and display order data

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
   return (
     <header>
       <Link to='/'>Fast React PIzza Co.</Link>
 
+      <SearchOrder />
       <p>Huzaifa</p>
     </header>
   );
