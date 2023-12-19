7965609acf90f12dcb4fe00ae8ae679e1c80d4b2
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-19 21:23:01 +0500

    Style add grid

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import Header from './Header';
-import CartOverview from '../features/cart/CartOverview';
-import { Outlet, useNavigation } from 'react-router-dom';
-import Loader from './Loader';
+import React from "react";
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
-   const navigation = useNavigation();
-   const isLoading = navigation.state === 'loading';
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
-    <div className='layout'>
+    <div className="grid h-screen  grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
-      <main>
-        <h1>Context</h1>
-        <Outlet /> 
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <h1>Context</h1>
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
