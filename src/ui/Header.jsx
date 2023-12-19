07a38e4a92bb28a218b819d4cb340b14fd4ebcb7
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-19 20:06:31 +0500

    Add basic colors style

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import SearchOrder from '../features/order/SearchOrder';
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
-    <header>
-      <Link to='/'>Fast React PIzza Co.</Link>
+    <header className="bg-yellow-500">
+      <Link to="/">Fast React PIzza Co.</Link>
 
       <SearchOrder />
       <p>Huzaifa</p>
@@ -13,4 +13,4 @@ function Header() {
   );
 }
 
-export default Header
+export default Header;
