fb2da5d38d3456a90c00d18979580a973db7a591
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-25 19:17:53 +0500

    Add selector functions in cart overview component

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,16 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  //   cart: [],
-  cart: [
-    {
-      pizzaId: 12,
-      name: "Mediterranean",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+    cart: [],
+//   cart: [
+//     {
+//       pizzaId: 12,
+//       name: "Mediterranean",
+//       quantity: 2,
+//       unitPrice: 16,
+//       totalPrice: 32,
+//     },
+//   ],
 };
 
 const cartSlice = createSlice({
@@ -52,3 +52,11 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+// ***** Selector Functions ***** //
+// Calculate the Number of items in the cart
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 import { addItem } from "../cart/cartSlice";
