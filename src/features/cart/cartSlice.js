a3d066ffd65850429eb151c3a85f482cf90293fb
huzaifaghazali <huzaifaghazali@gmail.com>
2023-12-25 22:33:12 +0500

    Add update items functionality

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -36,6 +36,8 @@ const cartSlice = createSlice({
 
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearCart(state) {
       state.cart = [];
@@ -64,6 +66,6 @@ export const getTotalCartQuantity = (state) =>
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
-// Check whether the pizza item is in the cart 
+// Check whether the pizza item is in the cart
 export const getCurrentQuantityById = (id) => (state) =>
   state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
